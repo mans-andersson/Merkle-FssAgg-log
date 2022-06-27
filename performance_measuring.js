ef12198385786744db15a3ac8a260c0aeca6d7d2
Måns Anderssone <manande@kth.se>
2022-06-27 15:19:37 +0200

    Add repetition of measurments

--- performance_measuring.js
+++ performance_measuring.js
@@ -2,24 +2,35 @@ const { Log } = require('./Log');
 const { performance } = require('perf_hooks');
 const fs = require('fs');
 const crypto = require('crypto');
-const repetitions = 1000000;
+const numEntires = 4000000;
+const repetitions = 1;
 
-const privateKey = crypto.createPrivateKey(fs.readFileSync('priv.pem'));
-const log = new Log(privateKey);
-// log.initFssAggMAC('pw', 'initMsg');
 
+let results = []
 let totalTime = 0;
 let startTime = 0;
 let endTime = 0;
-for (let index = 0; index < repetitions; index++) {
-    if (index % 10000 === 0) {
-        console.log(index);
+let log = undefined;
+
+for (let reps = 0; reps < repetitions; reps++) {
+    const privateKey = crypto.createPrivateKey(fs.readFileSync('priv.pem'));
+    log = new Log(privateKey);
+    // log = new Log();
+    // log.initFssAggMAC('pw', 'initMsg');
+    totalTime = 0;
+    for (let index = 0; index < numEntires; index++) {
+        if (index % 10000 === 0) {
+            console.log(index);
+        }
+        startTime = performance.now();
+        log.addEntry(JSON.stringify({a: index}));
+        endTime = performance.now();
+        totalTime += (endTime - startTime);
     }
-    startTime = performance.now();
-    log.addEntry(JSON.stringify(index));
-    endTime = performance.now();
-    totalTime += (endTime - startTime);
+    results.push(totalTime / numEntires);
 }
 
-console.log(`Time: ${totalTime / repetitions}`);
+console.log(`Times: ${results}`);
+const timeSum = results.reduce((a, b) => a + b, 0);
+console.log(`Time: ${timeSum/ repetitions}`);
 console.log(log.getEntries(0));

4dbbc7386f58b90642a873726400bd536f559a74
Måns Anderssone <manande@kth.se>
2022-12-12 15:13:57 +0100

    Update performance measuring

--- performance_proofs_fssagg.js
+++ performance_proofs_fssagg.js
@@ -1,18 +1,20 @@
 const { Log } = require('./Log');
 const Client = require('./Client');
 const { performance } = require('perf_hooks');
-const repetitions = 100;
+const math = require('mathjs');
+const repetitions = 31;
+const ignoredReps = 1;
 
 const log = new Log();
 log.initFssAggMAC('pw', 'initMsg');
-let numElements = 10000;
+let numElements = 4000000;
 
 for (let index = 0; index < numElements; index++) {
     log.addEntry(JSON.stringify({a: index}));
 }
 
 console.log('Log complete');
-let totalTime = 0;
+let results = [];
 let startTime = 0;
 let endTime = 0;
 let entries = log.getEntries(0);
@@ -21,8 +23,14 @@ for (let index = 0; index < repetitions; index++) {
     startTime = performance.now();
     Client.verifyFssAggMAC(mac, entries, 'pw');
     endTime = performance.now();
-    totalTime += (endTime - startTime);
+    if (index > ignoredReps) {
+        results.push(endTime - startTime);
+    }
+    console.log(index);
 }
 
-console.log(`Time: ${totalTime / repetitions}`);
+console.log(`Times: ${results}`);
+const timeSum = results.reduce((a, b) => a + b, 0);
+console.log(`Time: ${timeSum/ repetitions}`);
+console.log(`Standard deviation: ${math.std(results)}`)
 console.log(log.getEntries(0));
--- performance_proofs_merkle.js
+++ performance_proofs_merkle.js
@@ -3,34 +3,40 @@ const Client = require('./Client');
 const { performance } = require('perf_hooks');
 const { default: MerkleTree } = require('merkletreejs');
 const SHA256 = require('crypto-js/sha256');
-const repetitions = 100;
+const { start } = require('repl');
+const math = require('mathjs');
+const repetitions = 150;
+const skippedEarlyReps = 50;
+let numElements = 100000;
 
-const log = new Log();
-let numElements = 1000000;
+%NeverOptimizeFunction(main);
 
-let entryList = [];
-for (let index = 0; index < numElements; index++) {
-    entryList.push(JSON.stringify({a: index}));
+function main() {
+    const log = new Log();
+    for (let index = 0; index < numElements; index++) {
+        log.addEntry(JSON.stringify({a: index}));
+    }
+    const root = log.getRoot().toString('hex');
+
+    let results = [];
+    let startTime = 0;
+    let endTime = 0;
+    for (let index = 0; index < repetitions; index++) {
+        let randomIndex = 1;
+        startTime = performance.now();
+        let proof = log.getProofByIndex(randomIndex);
+        endTime = performance.now();
+        // let leaf = SHA256(log.getEntry(randomIndex));
+        // MerkleTree.verify(proof, leaf, root);
+        if (index >= skippedEarlyReps) {
+            results.push(endTime - startTime);
+        }
+    }
+    console.log(`Times: ${results}`);
+    console.log(`Results size ${results.length}`)
+    const timeSum = results.reduce((a, b) => a + b, 0);
+    console.log(`Time: ${timeSum/ (repetitions - skippedEarlyReps)}`);
+    console.log(`Standard deviation: ${math.std(results)}`)
 }
-log.tree = new MerkleTree(entryList.map(x => SHA256(x)), SHA256);
-log.dataStorage = entryList;
-log.numEntries = entryList.length;
-log.computeCommitment();
-console.log('Log ready');
 
-const root = log.getRoot().toString('hex');
-let totalTime = 0;
-let startTime = 0;
-let endTime = 0;
-for (let index = 0; index < repetitions; index++) {
-    let randomIndex = Math.floor(Math.random() * numElements);
-    let proof = log.getProofByIndex(randomIndex);
-    let leaf = SHA256(log.getEntry(randomIndex));
-    startTime = performance.now();
-    MerkleTree.verify(proof, leaf, root);
-    endTime = performance.now();
-    totalTime += (endTime - startTime);
-}
-
-console.log(`Time: ${totalTime / repetitions}`);
-console.log(log.getEntries(0));
+main();
